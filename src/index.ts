91fdee97e4ad1d93cbb7676dcccf08c3bc326eb0
isiagi <isiagigeofrey0@gmail.com>
2023-04-01 17:17:20 +0300

    refactor: Creates a class component

--- src/index.ts
+++ src/index.ts
@@ -3,40 +3,45 @@ import { Request, Response, NextFunction } from "express";
 
 type CustomRequest = Request & { user: {} };
 
-const getSignedJwtToken = async (
-  payload: {},
-  serect: string,
-  expire: number
-) => {
-  console.log("hello");
-  return JWT.sign(payload, serect, {
-    expiresIn: expire,
-  });
-};
+export class NodeAuth {
+  jwtSecret: any;
 
-const authenticationMiddleware = async (
-  req: CustomRequest,
-  res: Response,
-  next: NextFunction
-) => {
-  const authHeaders = req.headers.authorization;
-
-  let token = "";
-
-  if (!authHeaders || !authHeaders.startsWith("Bearer ")) {
-    return res.status(400).json({ message: "Invalid authorization" });
+  constructor(serect: string) {
+    this.jwtSecret = serect;
+    this.authenticationMiddleware = this.authenticationMiddleware.bind(this);
   }
 
-  token = authHeaders.split(" ")[1];
+  async authenticationMiddleware(
+    req: CustomRequest,
+    res: Response,
+    next: NextFunction
+  ) {
+    const authHeaders = req.headers.authorization;
 
-  try {
-    const decode = JWT.verify(token, process.env.JWT_SECRET);
+    let token = "";
 
-    req.user = { decode };
-    next();
-  } catch (error) {
-    res.status(400).json({ message: "Invalid authorization" });
+    if (!authHeaders || !authHeaders.startsWith("Bearer ")) {
+      return res.status(400).json({ message: "Please provide auth headers" });
+    }
+
+    token = authHeaders.split(" ")[1];
+
+    try {
+      const decode = JWT.verify(token, this.jwtSecret);
+
+      req.user = decode ;
+
+      next();
+      
+    } catch (error) {
+      res.status(400).json({ message: error });
+    }
   }
-};
 
-export { authenticationMiddleware, getSignedJwtToken };
+  async getSignedJwtToken(payload: {}, expire: number) {
+ 
+    return JWT.sign(payload, this.jwtSecret, {
+      expiresIn: expire,
+    });
+  }
+}
