548412ce770e3d50c1813c913329a6164f6217f3
isiagi <isiagigeofrey0@gmail.com>
2023-04-13 00:28:42 +0300

    feat: Add cookies

--- src/index.ts
+++ src/index.ts
@@ -26,24 +26,46 @@ export class NodeAuth {
       return res.status(400).json({ message: "Please provide auth headers" });
     }
 
-    token = authHeaders.split(" ")[1];
+    if (req.cookies.jwt) {
+      token = req.cookies.jwt;
+    } else {
+      token = authHeaders.split(" ")[1];
+    }
 
     try {
       const decode = JWT.verify(token, this.jwtSecret);
 
-      req.user = decode ;
+      req.user = decode;
 
       next();
-
     } catch (error) {
       res.status(400).json({ message: error });
     }
   }
 
   async getSignedJwtToken(payload: {}) {
- 
     return JWT.sign(payload, this.jwtSecret, {
       expiresIn: this.jwtExpires,
     });
+  }
+
+  async useCookieToken(res: Response, age: number = 5, stage: string = "dev") {
+    const token = this.getSignedJwtToken;
+
+    var date = new Date();
+    const cookieOptions: {
+      maxAge: number;
+      httpOnly: boolean;
+      secure?: boolean;
+    } = {
+      maxAge: date.setTime(date.getTime() + age * 60 * 1000), // defualt of 5 minutes
+      httpOnly: true,
+    };
+
+    if (stage === "production") cookieOptions.secure = true; // only for SSL in production
+
+    res.cookie("jwt", token, cookieOptions);
+
+    return res.status(200).json({ message: "success", token });
   }
 }
